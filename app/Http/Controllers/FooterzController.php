0e01a5ab47230da3a4759207fc6ce5ff14bbd50f
0xPurp <haroune1020@hotmail.com>
2021-09-29 14:08:50 +0200

    Ajout Controller Model Seeder & Migration (HomePage Features Services Portfolio Testimonials Team Contact Footerz)

---
+++ app/Http/Controllers/FooterzController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Footerz;
+use Illuminate\Http\Request;
+
+class FooterzController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Footerz  $footerz
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Footerz $footerz)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Footerz  $footerz
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Footerz $footerz)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Footerz  $footerz
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Footerz $footerz)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Footerz  $footerz
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Footerz $footerz)
+    {
+        //
+    }
+}
